36e1cec67d89f08b321523bddb085da3c4794d57
HH <hans.herzl@live.com>
2017-02-08 23:35:21 -0500

    Version 1.0.0-alpha-build22: package update

--- src/CatFactory.SqlServer/Extensions.cs
+++ src/CatFactory.SqlServer/Extensions.cs
@@ -16,10 +16,10 @@ namespace CatFactory.SqlServer
             => String.Format("[{0}]", value);
 
         public static String GetParameterName(this Column column)
-            => String.Format("@{0}", NamingConvention.GetCamelCase(column.Name));
+            => String.Format("@{0}", CodeNamingConvention.GetCamelCase(column.Name));
 
         public static String GetParameterName(this String name)
-            => String.Format("@{0}", NamingConvention.GetCamelCase(name));
+            => String.Format("@{0}", CodeNamingConvention.GetCamelCase(name));
 
         public static String GetProcedureName(this Table table, String action)
             => String.IsNullOrEmpty(table.Schema) ? String.Format("[{0}]", table.Name) : String.Format("[{0}].[{1}{2}]", table.Schema, table.Name, action);
